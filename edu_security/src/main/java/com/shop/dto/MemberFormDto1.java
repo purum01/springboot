9ccf837a535482569e042cb0a0f1009ba36d3f4f
purum01 <purumskyamy@gmail.com>
2024-08-16 23:07:16 +0900

    Add files via upload

---
+++ edu_security/src/main/java/com/shop/dto/MemberFormDto1.java
@@ -0,0 +1,16 @@
+package com.shop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MemberFormDto1 {
+	private String name;
+	private String email;
+	private String password;
+	private String address;
+
+}
