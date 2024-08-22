3cbbd537f617a77b8f4a1cc793bc02240c7bf5bf
purum01 <purumskyamy@gmail.com>
2024-08-23 07:52:27 +0900

    Delete edu_service directory

--- edu_service/src/main/java/com/edu/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.edu.repository;
-
-import com.edu.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
